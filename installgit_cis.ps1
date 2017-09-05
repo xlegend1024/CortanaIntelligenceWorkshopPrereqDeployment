becc39c82411a3c233a16a749d1c062a00e0cc63
Hyun <hyunsuk.shin@live.com>
2017-09-05 14:41:58 -0700

    Add files via upload

--- installgit_cis.ps1
+++ installgit_cis.ps1
@@ -8,8 +8,12 @@ $file = "$storageDir\Git-2.14.1-64-bit.exe"
 $webclient.DownloadFile($gitUrl,$file)
 
 $p = Start-Process $file -ArgumentList $arg -wait -NoNewWindow -PassThru
-
 $p.HasExited
-
 $p.ExitCode
+
+$webappUrl = "https://github.com/xlegend1024/AdventureWorksTravel.git"
+$cloneDir = "C:\myWebApp"
+$gitP = Start-Process "git.exe" -ArgumentList "clone $webappUrl --separate-git-dir=$cloneDir" 
+$gitP.HasExited
+$gitP.ExitCode
 
