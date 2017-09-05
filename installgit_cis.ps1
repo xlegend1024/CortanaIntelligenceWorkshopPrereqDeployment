aba069b60f822f42a2daad0f44594ed305b7481a
Hyun <hyunsuk.shin@live.com>
2017-09-05 16:24:05 -0700

    Add files via upload

--- installgit_cis.ps1
+++ installgit_cis.ps1
@@ -5,16 +5,25 @@ $arg='/SILENT'
 
 $webclient = New-Object System.Net.WebClient
 $file = "$storageDir\Git-2.14.1-64-bit.exe"
+$dataUrl="https://hyssh0mtc0sea.blob.core.windows.net/awtworkshopdata/AWT.CSV"
+$datafile = "C:\data\1.MachineLearning\awt_flightsandweather.csv"
+$fnwUrl = "http://aka.ms/flightsandweather"
+$flightandweatherfile = "C:\data\flightsandweather.csv"
 $webclient.DownloadFile($gitUrl,$file)
+$webclient.DownloadFile($dataUrl,$datafile)
+$webclient.DownloadFile($fnwUrl,$flightandweatherfile)
 
-$p = Start-Process $file -ArgumentList $arg -wait -NoNewWindow -PassThru
-$p.HasExited
-$p.ExitCode
 
 $webappUrl = "https://github.com/xlegend1024/AdventureWorksTravel.git"
 $cloneDir = "C:\myWebApp"
 $gitArg = "clone $webappUrl --separate-git-dir=$cloneDir"
+
+$p = Start-Process $file -ArgumentList $arg -wait -NoNewWindow -PassThru
 $gitP = Start-Process "git.exe" -ArgumentList $gitArg
+
+$p.HasExited
 $gitP.HasExited
+
 $gitP.ExitCode
+$p.ExitCode
 
