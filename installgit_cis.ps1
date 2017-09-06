0727275da5247edc2b21db4f53144f0c4f16bd5b
Hyun <hyunsuk.shin@live.com>
2017-09-06 09:08:33 -0700

    Add files via upload

--- installgit_cis.ps1
+++ installgit_cis.ps1
@@ -1,32 +1,36 @@
 # Azure CIS Workshop Dev VM
 
+
+#Download git
 $gitUrl = "https://github.com/git-for-windows/git/releases/download/v2.14.1.windows.1/Git-2.14.1-64-bit.exe"
 $storageDir = $env:TEMP
 $arg = '/SILENT'
-
 $webclient = New-Object System.Net.WebClient
 $file = "$storageDir\Git-2.14.1-64-bit.exe"
 $webclient.DownloadFile($gitUrl, $file)
-$gitpath = "C:\Program Files\Git\bin\git.exe"
 
-$Location = "C:\data\"
-New-Item -Path $Location -ItemType "directory"
-$fnwUrl = "http://aka.ms/flightsandweather"
-$flightandweatherfile = "C:\data\flightsandweather.csv"
-$webclient.DownloadFile($fnwUrl, $flightandweatherfile)
-
+#Download ML Training data
 $Location = "C:\data\1.MachineLearning\"
 New-Item -Path $Location -ItemType "directory"
 $dataUrl = "https://hyssh0mtc0sea.blob.core.windows.net/awtworkshopdata/AWT.CSV"
 $datafile = "C:\data\1.MachineLearning\awt_flightsandweather.csv"
 $webclient.DownloadFile($dataUrl, $datafile)
 
+#Download ML Batch data
+$Location = "C:\data\"
+New-Item -Path $Location -ItemType "directory"
+$fnwUrl = "http://aka.ms/flightsandweather"
+$flightandweatherfile = "C:\data\flightsandweather.csv"
+$webclient.DownloadFile($fnwUrl, $flightandweatherfile)
+
+#Download ADF Data Movement Gateway msi
 $Location = "C:\ADF_Gateway\"
 New-Item -Path $Location -ItemType "directory"
 $admgUrl = "https://download.microsoft.com/download/E/4/7/E4771905-1079-445B-8BF9-8A1A075D8A10/DataManagementGateway_2.12.6414.2%20(64-bit).msi"
 $admgfile = "C:\ADF_Gateway\DataManagementGateway_2.12.6414.2 (64-bit).msi"
 $webclient.DownloadFile($admgUrl, $admgfile)
 
+#Download PowerBI msi
 $Location = "C:\temp\"
 New-Item -Path $Location -ItemType "directory"
 $pbiUrl = "http://go.microsoft.com/fwlink/?LinkID=521662"
@@ -34,15 +38,19 @@ $pbiFile = "C:\temp\PowerBIDesktop.msi"
 $webclient.DownloadFile($pbiUrl, $pbiFile)
 $pbiArg = "ACCEPT_EULA=1 /quiet"
 
+#Install git
+$p = Start-Process $file -ArgumentList $arg -wait -NoNewWindow -PassThru
+#Install PowerBI
+$pbiP = Start-Process $pbiFile -ArgumentList $pbiArg
+
+#Clone Web App
 $Location = "C:\myWebApp\"
 New-Item -Path $Location -ItemType "directory"
 $webappUrl = "https://github.com/xlegend1024/AdventureWorksTravel.git"
-$cloneDir = "C:\myWebApp\"
-$gitArg = "clone $webappUrl --separate-git-dir=$cloneDir"
-
-$p = Start-Process $file -ArgumentList $arg -wait -NoNewWindow -PassThru
-$pbiP = Start-Process $pbiFile -ArgumentList $pbiArg
+$gitpath = "C:\Program Files\Git\bin\git.exe"
+$gitArg = "clone $webappUrl $Location"
 $gitP = Start-Process $gitpath -ArgumentList $gitArg
+
 <#
 $p.HasExited
 $pbiP.HasExited
@@ -52,8 +60,5 @@ $gitP.ExitCode
 $pbiP.ExitCode
 $p.ExitCode
 #>
-
-
-
 
 
