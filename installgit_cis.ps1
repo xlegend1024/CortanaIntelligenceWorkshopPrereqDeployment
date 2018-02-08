02c11b3d4d32222bf7cf1230abf3559add4d112f
Hyun Suk Shin (SEATTLE MTC) <hyssh@microsoft.com>
2018-02-08 11:04:53 -0800

    update ARM template and PS script

--- installgit_cis.ps1
+++ installgit_cis.ps1
@@ -4,11 +4,13 @@
 $webclient = New-Object System.Net.WebClient
 
 #Download git
+<#
 $gitUrl = "https://github.com/git-for-windows/git/releases/download/v2.14.1.windows.1/Git-2.14.1-64-bit.exe"
 $storageDir = $env:TEMP
 $arg = '/SILENT'
 $file = "$storageDir\Git-2.14.1-64-bit.exe"
 $webclient.DownloadFile($gitUrl, $file)
+#>
 
 #Download ML Training data
 $Location = "C:\data\1.MachineLearning\"
@@ -39,17 +41,21 @@ $admgfile = "C:\ADF_Gateway\DataManagementGateway_2.12.6414.2 (64-bit).msi"
 $webclient.DownloadFile($admgUrl, $admgfile)
 
 #Download PowerBI msi
+<#
 $Location = "C:\temp\"
 New-Item -Path $Location -ItemType "directory"
 $pbiUrl = "http://go.microsoft.com/fwlink/?LinkID=521662"
 $pbiFile = "C:\temp\PowerBIDesktop.msi"
 $webclient.DownloadFile($pbiUrl, $pbiFile)
 $pbiArg = "ACCEPT_EULA=1 /quiet"
+#>
 
 #Install git
+<#
 $p = Start-Process $file -ArgumentList $arg -wait -NoNewWindow -PassThru
 #Install PowerBI
 $pbiP = Start-Process $pbiFile -ArgumentList $pbiArg
+#>
 
 #Clone Web App
 $Location = "C:\myWebApp\"
@@ -65,10 +71,12 @@ $gitP = Start-Process $gitpath -ArgumentList $gitmail
 $gitP = Start-Process $gitpath -ArgumentList $gitname
 
 #Download FireFox
+<#
 $foxUrl = "https://download.mozilla.org/?product=firefox-stub&os=win&lang=en-US&attribution_code=c291cmNlPXd3dy5iaW5nLmNvbSZtZWRpdW09cmVmZXJyYWwmY2FtcGFpZ249KG5vdCBzZXQpJmNvbnRlbnQ9KG5vdCBzZXQpJnRpbWVzdGFtcD0xNTA0NzE2MDY1&attribution_sig=7f33be39c4986e04cb16f6a41e83e170e6657778642d9fc71ee6b58a1cab1550"
 $foxFile = "C:\temp\firefox.exe"
 $webclient.DownloadFile($foxUrl, $foxFile)
 $foxP = Start-Process $foxFile 
+#>
 
 #http://www.google.com/chrome/eula.html?system=true&standalone=1&platform=win64
 
